de7c3935b5b6cbcd69b0e4258d046fcf60a52804
menglingyu659 <50974543+menglingyu659@users.noreply.github.com>
2021-11-21 23:35:27 +0800

    a

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,12 +5,18 @@ import Message from "@/views/Message";
 import Project from "@/views/Project";
 import BlogDetail from "@/views/Blog/Detail";
 
+const p = "/my-site/dist";
+
 export default [
-  { name: "Home", path: "/", component: Home },
-  { name: "Blog", path: "/article", component: Blog },
-  { name: "CategoryBlog", path: "/article/cate/:categoryId", component: Blog },
-  { name: "BlogDetail", path: "/article/:id", component: BlogDetail },
-  { name: "About", path: "/about", component: About },
-  { name: "Message", path: "/message", component: Message },
-  { name: "Project", path: "/project", component: Project },
+  { name: "Home", path: p + "/", component: Home },
+  { name: "Blog", path: p + "/article", component: Blog },
+  {
+    name: "CategoryBlog",
+    path: p + "/article/cate/:categoryId",
+    component: Blog,
+  },
+  { name: "BlogDetail", path: p + "/article/:id", component: BlogDetail },
+  { name: "About", path: p + "/about", component: About },
+  { name: "Message", path: p + "/message", component: Message },
+  { name: "Project", path: p + "/project", component: Project },
 ];
